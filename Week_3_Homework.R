e250b4ee092d4d315196012d89eb8c39592d6ac8
Daniel Harper <danie@campus-037-138.ucdavis.edu>
2024-10-17 13:18:09 -0700

    This hw was good and made me realize that I need to try and remember to fill in the (, 2) so that is recalls what it is I am looking for. I had to resubmit this as a new file, sorry if it is repetitive.

--- Week_3_Homework.R
+++ Week_3_Homework.R
@@ -1,34 +1,3 @@
-?read.csv
-surveys <- read.csv("data/portal_data_joined.csv")
-nrow(surveys)
-ncol(surveys)
-dim(surveys)
-?str
-str(surveys)
-surveys
-class(surveys)
-summary(surveys)
-str(surveys)
-
-head(surveys)
-tail(surveys)
-head(surveys,n = 3)
-
-summary(object = surveys)
-summary.data.frame(surveys)
-?summary.data.frame
-
-surveys[1,5]
-head(surveys,1)
-
-surveys[,1]
-surveys[1,]
-
-#install.packages('tidyverse')
-#library(tidyverse)
-
-##
-
 surveys <- read.csv("data/portal_data_joined.csv")
 colnames(surveys)
 surveys_base <- surveys[1:5000, c(6, 9, 13)]
